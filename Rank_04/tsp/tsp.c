4bc48439c5f5de9932e498c33d00e445bb4b34f7
dtorretta <danielatorretta18@gmail.com>
2024-12-12 02:35:23 +0100

    subject added

--- Rank_04/tsp/tsp.c
+++ Rank_04/tsp/tsp.c
@@ -1,3 +1,66 @@
+/*
+Assignment name			: tsp
+Expected files			: *.c *.h
+Allowed functions   : write, sqrtf, getline, fseek, fscanf, ferror, feof, fabsf, memcpy, fprintf, fclose, malloc, calloc, realloc, free, fopen, errno, stderr, stdin, stdout
+-------------------------------------------------------
+
+The first publication referring to this problem as the "traveling salesman 
+problem" is found in the 1949 RAND Corporation report by Julia Robinson, 
+"On the Hamiltonian game (a traveling salesman problem)."
+
+Here is how she defines the problem:
+
+"The purpose of this note is to give a method for solving a problem related
+to the traveling salesman problem. It seems worthwhile to give a description
+of the original problem. One formulation is to find the shortest route for a
+salesman starting from Washington, visiting all the state capitals and then
+returning to Washington.
+
+More generally, to find the shortest CLOSED CURVE containing n given points 
+in the plane."
+
+for example with the following set of cities:
+0, 0
+1, 0
+2, 0
+0, 1
+1, 1
+2, 1
+1, 2
+2, 2
+which can be presented as follows:
++ + +
++ + +
+  + +
+the shortest path is:
+ _____
+|__   |
+   |__|
+
+so you should print the length of this path that is:
+8.00
+
+Write a program that will read a set of city coordinates in the form 
+'%f, %f\n' from the standard input and will print the length of the shortest
+possible path containing all these cities under the form '%.2f'.
+
+Your program will not be tested with more than 11 cities.
+
+You will find in this directory a file tsp.c containing all the boring parts of
+this exercise and example files to help you test your program.
+
+hint: in order to use sqrtf, add -lm at the end of your compilation command.
+
+For example this should work:
+$> cat square.txt
+1, 1
+0, 1
+1, 0
+0, 0
+$> ./tsp < square.txt | cat -e
+4.00$
+*/
+
 #include <stdio.h>
 #include <errno.h>
 #include <malloc.h>
@@ -24,7 +87,6 @@ void swap(float *x0, float *x1, float *y0, float *y1)
     *x1 = tempx;
     *y1 = tempy;
 }
-
 float find_shortest (float *x, float *y, int len)
 {
     int i;
@@ -33,11 +95,10 @@ float find_shortest (float *x, float *y, int len)
     float min_distance;
     float temp_distance;
     float total_distance;
-    int start;
     
-    start = 0; //punto de partida
-    //si solo hay 2 ciudades, la ditancia total es ida y vuelta
-    if(start == len - 1)
+    j = 0; //punto de partida
+    
+    if(j == len - 1) //si solo hay 2 ciudades, la ditancia total es ida y vuelta
     {
         total_distance = distance(x[0], y[0], x[1], y[1]);
         total_distance *= 2;
@@ -45,36 +106,37 @@ float find_shortest (float *x, float *y, int len)
     }
     else
     {
-        j = 0;
-        //si tengo 4 ciudades incluido el punto de partida (0 1 2 3) --> len 3 --> j puede llegar hasta 2
+        //si len es 3 --> max j posible es 2
         //j=0 (0 1) calcula la distancia entre el punto de partida y la ciudad 1
         //j=1 (1 2) calcula la distancia entre ciudad 1 y la ciudad 2
         //j=2 (2 3) calcula la distancia entre ciudad 2 y la ciudad 3
         while(j < len) 
         {
             min_distance = INFINITY;
-            //si tengo 4 ciudades (0 1 2 3) --> len 3 --> (start + i) puede llegar hasta 3
-            i = 1; //empieza en 1 para comparar con la siguiente ciudad que no sea el punto de partida
-            while ((start + i) <= len)
+            //si len es 3 --> i puede llegar hasta 3
+            i = j + 1; //empieza en 1 para comparar con la siguiente ciudad que no sea el punto de partida
+            while (i <= len)
             {
-                temp_distance = distance(x[start], y[start], x[start + i], y[start + i]); //comparo start con cada uno de los elementos
+                temp_distance = distance(x[j], y[j], x[i], y[i]); //comparo start(j) con cada uno de los elementos
                 if(temp_distance < min_distance)
                 {
                     min_distance = temp_distance;
-                    next = start + i;
+                    next = i;
                 }
                 i++;
             }
             total_distance += min_distance; 
-            //ordeno las ciudades para que despues de start (start + 1) este la ciudad mas cerca (next)
-            swap(&x[start + 1], &x[next], &y[start + 1], &y[next]);
-            j++;
-            start++; //mi nuevo punto de partida es next;
+            //al principio 'j' es el punto de partida
+            //next es la ciudad mas cercana a 'j' que debe reordenarse y quedar consecutiva a esta
+            //para ello hago swap de 'j + 1' y 'next'
+            swap(&x[j + 1], &x[next], &y[j + 1], &y[next]);
+            j++; //mi nuevo punto de partida es 'next' que ahora esta posicionado en 'j + 1'
         }
         total_distance += distance(x[len], y[len], x[0], y[0]); //vuelta al punto de partida
         return(total_distance);
     }
 }
+
 int main (int ac, char **av)
 {
     (void) ac;
@@ -91,7 +153,7 @@ int main (int ac, char **av)
     {
         i++;    
     }
-    len = i - 1;
+    len = i - 1; //len es la cantidad de ciudades SIN contar el punto de partida
     total_distance = find_shortest (x, y, len);
     printf("total distance: %.2f\n", total_distance);
     return(0);
